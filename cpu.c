80bc6c5cf777e59512c3d6095614beac75b61186
Ronald Waite <waiteforme15@gmail.com>
2015-12-02 11:03:54 +0900

    Implement all opcodes ending in 0b01.

--- cpu.c
+++ cpu.c
@@ -43,23 +43,28 @@ unsigned step(struct state *s, byte (*read)(word), void (*write)(word, byte)) {
     word l = 0;
     word h = 0;
     unsigned cycles = 0; //the number of cycles used to complete the instruction
+    unsigned static_cycle = 0;
 
     byte opcode = read(s->PC++);
+    byte aaa = (opcode >> 5) & 0x7;
+    byte bbb = (opcode >> 2) & 0x7;
+    byte cc = (opcode) & 0x3;
 
     // opcode is read as aaabbbcc with cc bits being read first to determine
     // which instruction to run accordingly
-    switch((opcode) & 0x3) {
+    switch(cc) {
         case 0x1:
             goto CC01;
-        default:
-            break;
+        case 2:
+            goto CC10;
+        case 0:
+            goto CC00;
     }
 
     // use these switch statements when cc is 01
 CC01:
-    int static_cycle = 0;
     // check addressing mode
-    switch((opcode >> 2) & 0x7) {
+    switch(bbb) {
         case 2: // immediate
             m = s->PC++;
             cycles = 2;
@@ -70,7 +75,7 @@ CC01:
             break;
         case 5: // zero page, x
             m = (read(s->PC++) + s->X) & 0xFF;
-            cycles = 4;
+            cycles = (aaa < 2) ? 3 : 4;
             break;
         case 0: // (indirect, x)
             M = read(s->PC++);
@@ -113,7 +118,7 @@ CC01:
     M = read(m);
 
     // check instruction
-    switch((opcode >> 5) & 0x7) {
+    switch(aaa) {
         case 0:
             goto ORA;
         case 1:
@@ -123,7 +128,7 @@ CC01:
         case 0x3: 
             goto ADC;
         case 4:
-            cycles = static_cycle;
+            cycles = static_cycle != 0 ? static_cycle : cycles;
             goto STA;
         case 5:
             goto LDA;
@@ -144,38 +149,93 @@ ADC:
     return cycles;
 
 ORA:
-    A = A | M;
+    s->A = s->A | M;
     s->N = (BIT(s->A,7));
     s->Z = (t==0) ? 1 : 0;
     return cycles;
 
 AND:
-    A = A & M;
+    s->A = s->A & M;
     s->N = (BIT(s->A,7));
     s->Z = (t==0) ? 1 : 0;
     return cycles;
 
 EOR:
-    A = A & M;
+    s->A = s->A & M;
     s->N = (BIT(s->A,7));
     s->Z = (t==0) ? 1 : 0;
     return cycles;
 
 STA:
-    write(m, A);
+    write(m, s->A);
     return cycles;
 
 LDA:
+    s->A = M;
+    s->N = (BIT(s->A,7));
+    s->Z = (t==0) ? 1 : 0;
     return cycles;
 
 CMP:
+    t = s->A - M;
+    s->N = (BIT(t,7));
+    s->C = (s->A >= M) ? 1 : 0;
+    s->Z = (t==0) ? 1 : 0;
     return cycles;
 
 SBC:
-
+    t = s->A - M - s->C;
+    s->V = (t>127 || t < -128) ? 1 : 0;
+    s->C = (t >= 0) ? 1 : 0;
+    s->N = (BIT(t, 7));
+    s->Z = (t==0) ? 1 : 0;
+    s->A = t & 0xFF;
     return cycles;
 
 CC10:
+    switch(bbb) {
+        case 0: // immediate
+            m = s->PC++;
+            cycles = 2;
+            break;
+        case 1: // zero page
+            m = read(s->PC++);
+            cycles = (aaa == 5 || aaa == 4) ? 3 : 5;
+            break;
+        case 2: // accumulator
+            cycles = 2;
+            break;
+        case 3: // absolute
+            h = read(s->PC++) << 8;
+            m = h | read(s->PC++);
+            cycles = (aaa == 5 || aaa == 4) ? 4 : 6;
+            break;
+        case 5: // zero page,x or zero page,y for STX and LDX
+            cycles = 6;
+            if (aaa == 4 || aaa == 5) {
+                m = (read(s->PC++) + s->Y) & 0xFF;
+                cycles = 4;
+            } else {
+                m = (read(s->PC++) + s->X) & 0xFF;
+            }
+            break;
+        case 7: // absolute,x or absolute,y for LDX
+            l = read(s->PC++);
+            h = read(s->PC++) << 8;
+            m = h | l;
+            if (aaa == 5) {
+                m = m + s->X;
+                cycles = ((((m + s->Y) >> 8) & 0xFF ) == ((h >> 8) & 0xFF)) ? 4 : 5;
+            } else {
+                m = m + s->X;
+                cycles = 7;
+            }
+            break;
+        default:
+            break;
+    }
+
+    M = read(m);
 
 CC00:
  
